7aca81b9dd9bb6d75842895f8f52b19315327770
Richard Amodia <richard.amodia@mrpau.com>
2018-10-05 17:09:24 +0800

    Temp commit.

--- .buildkite/sign_windows_installer.bat
+++ .buildkite/sign_windows_installer.bat
@@ -6,7 +6,7 @@ rem After the installer successfully sign it will upload the installer at the Si
 IF NOT "%SIGN_WINDOWS_INSTALLER%" == "true" (GOTO DONT_SIGN)
 
 set current_path=%cd%
-buildkite-agent.exe artifact download "dist/*.exe" . --step "Build kolibri windows installer" --build %BUILDKITE_BUILD_ID% --agent-access-token %BUILDKITE_AGENT_ACCESS_TOKEN%
+buildkite-agent.exe artifact download "dist/*.exe" . --step "Build Windows installer" --build %BUILDKITE_BUILD_ID% --agent-access-token %BUILDKITE_AGENT_ACCESS_TOKEN%
 %WINDOWS_SIGN_SCRIPT_PATH% "%current_path%\dist" && cd "%current_path%\dist\" && buildkite-agent.exe artifact upload "*.exe"
 GOTO END
 
