238b92531c0c4fa2a08a220982fe03d768c7737c
Richard Amodia <richard.amodia@mrpau.com>
2018-10-08 19:10:47 +0800

    Temp commit.

--- .buildkite/sign_windows_installer.bat
+++ .buildkite/sign_windows_installer.bat
@@ -3,14 +3,6 @@ rem To sign the Windows installer, set the `SIGN_WINDOWS_INSTALLER=true` environ
 rem This will download the Kolibri Windows installer artifact at the windows-2016 Buildkite agent.
 rem After the installer successfully sign it will upload the installer at the Sign Windows Installer pipeline artifact.
 
-IF NOT "%SIGN_WINDOWS_INSTALLER%" == "true" (GOTO DONT_SIGN)
-
 set current_path=%cd%
 buildkite-agent.exe artifact download "dist/*.exe" . --step "Build Windows installer" --build %BUILDKITE_BUILD_ID% --agent-access-token %BUILDKITE_AGENT_ACCESS_TOKEN%
 %WINDOWS_SIGN_SCRIPT_PATH% "%current_path%\dist" && cd "%current_path%\dist\" && ren *.exe *-.exe && buildkite-agent.exe artifact upload "*.exe"
-GOTO END
-
-:DONT_SIGN
-echo Set the SIGN_WINDOWS_INSTALLER=true environment variable to sign the Windows installer.
-
-:END
